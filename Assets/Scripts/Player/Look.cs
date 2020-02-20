4b5d0c6183200787240b76d2df270c378516226a
Yasuhiro <arseniy2003bulgakov@gmail.com>
2020-02-20 23:24:23 +0300

    Unity Initial commit

---
+++ Assets/Scripts/Player/Look.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Yasuhiro.FPSGame
+{
+    public class Look : MonoBehaviour
+    {
+        public Transform player;
+        public Transform cams;
+        public float xSensivity;
+        public float ySensivity;
+        private float maxAngle = 90;
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            SetY();
+        }
+
+        void SetY() {
+            float _input = Input.GetAxis("Mouse Y") * ySensivity * Time.deltaTime;
+            Quaternion _adj = Quaternion.AngleAxis(_input, -Vector3.right);
+            Quaternion _delta = cams.localRotation * _adj;
+            cams.localRotation = _delta;
+        }
+    }
+}
+
