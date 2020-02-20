019aa1cc4e7c90ad792119662b020b3b2d367844
Yasuhiro <arseniy2003bulgakov@gmail.com>
2020-02-21 00:19:06 +0300

    Control Update + Design

--- Assets/Scripts/Player/Look.cs
+++ Assets/Scripts/Player/Look.cs
@@ -7,26 +7,58 @@ namespace Yasuhiro.FPSGame
 {
     public class Look : MonoBehaviour
     {
+        public static bool cursorIsLocked = true;
         public Transform player;
         public Transform cams;
-        public float xSensivity;
-        public float ySensivity;
+        public float Sensivity;
         private float maxAngle = 90;
+        private Quaternion camCentre;
         void Start()
         {
-
+            camCentre = cams.localRotation;
         }
 
         void Update()
         {
             SetY();
+            SetX();
+            UpdateCursorLock();
         }
 
         void SetY() {
-            float _input = Input.GetAxis("Mouse Y") * ySensivity * Time.deltaTime;
-            Quaternion _adj = Quaternion.AngleAxis(_input, -Vector3.right);
+            float _input = Input.GetAxis("Mouse Y") * Sensivity * Time.deltaTime;
+            Quaternion _adj = Quaternion.AngleAxis(_input, Vector3.left);
             Quaternion _delta = cams.localRotation * _adj;
-            cams.localRotation = _delta;
+
+            if (Quaternion.Angle(camCentre, _delta) < maxAngle) {
+                cams.localRotation = _delta;
+            }
+        }
+
+        void SetX() {
+            float _input = Input.GetAxis("Mouse X") * Sensivity * Time.deltaTime;
+            Quaternion _adj = Quaternion.AngleAxis(_input, Vector3.up);
+            Quaternion _delta = player.localRotation * _adj;
+
+            player.localRotation = _delta;
+        }
+
+        void UpdateCursorLock() {
+            if (cursorIsLocked) {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+                if (Input.GetKeyDown(KeyCode.Escape)) {
+                    cursorIsLocked = false;
+                }
+            } else {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                if (Input.GetKeyDown(KeyCode.Escape)) {
+                    cursorIsLocked = true;
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -17,8 +17,8 @@ namespace Yasuhiro.FPSGame {
         // Update is called once per frame
         void FixedUpdate()
         {
-            float _hMove = Input.GetAxis("Horizontal");
-            float _vMove = Input.GetAxis("Vertical");
+            float _hMove = Input.GetAxisRaw("Horizontal");
+            float _vMove = Input.GetAxisRaw("Vertical");
             Vector3 _direction = new Vector3(_hMove, 0, _vMove);
             _direction.Normalize();
             player_rig.velocity = transform.TransformDirection(_direction) * moveSpeed * Time.fixedDeltaTime;
